b3716592fe50a7dd8ce36f7361cc43a94bad6ecf
Ryan Mundy <ryanmundy34@gmail.com>
2018-11-09 15:52:12 -0600

    created employee class and empty employee array

--- client.js
+++ client.js
@@ -1,5 +1,15 @@
 console.log('js is loaded');
 
+class Employee {
+    constructor(firstName, lastName, id, title, annualSalary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.title = title;
+        this.annualSalary = annualSalary;
+    }//end constructor
+}//end Item class
+
 let employees = [];
 
 $(document).ready( readyNow );

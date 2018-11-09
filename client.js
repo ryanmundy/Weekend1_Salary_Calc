4fcb88b5088cffd5f30c4fa88bc06c46dc4cca1a
Ryan Mundy <ryanmundy34@gmail.com>
2018-11-09 15:45:30 -0600

    created addEmployee function to be called on button click, button click working

--- client.js
+++ client.js
@@ -1,8 +1,16 @@
 console.log('js is loaded');
- 
+
+let employees = [];
+
 $(document).ready( readyNow );
 
 function readyNow(){
     console.log('in jquery');
-    $()
+    $('#addEmployeeButton').on('click', addEmployee);
 }//end ready now
+
+function addEmployee(){
+    console.log('in addEmployee');
+    
+    
+}//end add employee
